b8330713bc369f4677aa16ba86cab6ece556d96f
PatrykSol <s1142288@student.hsleiden.nl>
2023-12-06 19:12:16 +0100

    Initial commit

---
+++ src/main/java/com/example/webshopapi/model/Product.java
@@ -0,0 +1,87 @@
+package com.example.webshopapi.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "price")
+    private double price;
+
+    @Column(name = "stockquantity")
+    private int stockquantity;
+
+    @Column(name = "categoryid")
+    private int categoryid;
+
+    @Column(name = "manufacturer")
+    private String manufacturer;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getStockquantity() {
+        return stockquantity;
+    }
+
+    public void setStockquantity(int stockquantity) {
+        this.stockquantity = stockquantity;
+    }
+
+    public int getCategoryid() {
+        return categoryid;
+    }
+
+    public void setCategoryid(int categoryid) {
+        this.categoryid = categoryid;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+}
